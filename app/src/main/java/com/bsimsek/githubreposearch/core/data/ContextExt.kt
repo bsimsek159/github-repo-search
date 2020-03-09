e6c1ce2225639731f0a2f2c77fcc8bce8ce6811b
Batu Simsek <bsimsek159@gmail.com>
2020-03-09 21:09:38 +0300

    add extension, launch screen

---
+++ app/src/main/java/com/bsimsek/githubreposearch/core/data/ContextExt.kt
@@ -0,0 +1,10 @@
+package com.bsimsek.githubreposearch.core.data
+
+import android.content.Context
+import android.net.ConnectivityManager
+
+fun Context.isNetworkActive(): Boolean {
+    val connectivityManager = this.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    val networkInfo = connectivityManager.activeNetworkInfo
+    return networkInfo != null && networkInfo.isConnected
+}
---
+++ app/src/main/java/com/bsimsek/githubreposearch/core/data/ResponseExt.kt
@@ -0,0 +1,15 @@
+package com.bsimsek.githubreposearch.core.data
+
+import org.json.JSONObject
+import retrofit2.Response
+
+fun Response<*>.toErrorMessage() : String{
+    val code = this.code().toString()
+    val message = try {
+        val jObjError = JSONObject(this.errorBody()?.string() ?: "empty body")
+        jObjError.getJSONObject("error").getString("message")
+    } catch (e: Exception) {
+        e.message
+    }
+    return if (message.isNullOrEmpty()) " error code = $code " else " error code = $code  & error message = $message "
+}
