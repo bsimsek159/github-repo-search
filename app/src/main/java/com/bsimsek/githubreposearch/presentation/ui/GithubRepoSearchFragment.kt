64d5bb033e8c0e02fcacb1e493facb949eec8ab0
Batu Simsek <bsimsek159@gmail.com>
2020-03-10 09:13:42 +0300

    refactor

--- app/src/main/java/com/bsimsek/githubreposearch/presentation/ui/GithubRepoSearchFragment.kt
+++ app/src/main/java/com/bsimsek/githubreposearch/presentation/ui/GithubRepoSearchFragment.kt
@@ -31,7 +31,6 @@ class GithubRepoSearchFragment : BaseFragment<GithubRepoSearchViewModel>() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         observeData()
         setSearchListener()
     }
--- app/src/main/java/com/bsimsek/githubreposearch/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/bsimsek/githubreposearch/presentation/ui/MainActivity.kt
@@ -20,7 +20,6 @@ class MainActivity : BaseActivity(), HasSupportFragmentInjector {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
         supportFragmentManager.beginTransaction()
             .replace(R.id.container, GithubRepoSearchFragment()).commit()
     }
